4b4d165b5266aff6b57bc55fa59e8908b8ac0d66
Manar Alhamedi <maa356@mail.aub.edu>
2023-04-22 01:33:22 +0300

    Add getItemByItemType for the API

--- api/menu/menu.router.js
+++ api/menu/menu.router.js
@@ -1,8 +1,9 @@
-const {createItem,  getItems, getItemByItemName, updateItem, deleteItem} = require("./menu.controller");
+const {createItem,  getItems, getItemByItemName, getItemByItemType, updateItem, deleteItem} = require("./menu.controller");
 const router = require("express").Router();
 router.post("/", createItem);
 router.get("/",  getItems);
 router.get("/:ItemName", getItemByItemName);
+router.get("/type/:ItemType", getItemByItemType);
 router.patch("/", updateItem);  
 router.delete("/", deleteItem);  //take the data from the body
 
